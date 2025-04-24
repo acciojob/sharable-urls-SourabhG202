396b9f93ec4717e41a43a640bac7ead1013f9b0c
Sourabh G <154436409+SourabhG202@users.noreply.github.com>
2025-04-24 19:15:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+const button = document.getElementById("button");
+const urlElement = document.getElementById("url");
+
+button.addEventListener("claick",function(){
+	const name = document.getElementById("name");
+	const year = document.getElementById("year");
+	const newUrl = `https://localhost:8080/?name=${name}&year=${year}`;
+	urlElement.textContent = newUrl;
+})
